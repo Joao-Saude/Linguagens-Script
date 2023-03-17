2193d27ae2795acb16d68b24508f8c28c8d4f8bc
João Saúde <107185975+Joao-Saude@users.noreply.github.com>
2023-03-17 16:09:48 +0000

    Add files via upload

---
+++ FichaPratica_01/Ficha1/js/Ficha1.js
@@ -0,0 +1,30 @@
+'use strict'; 
+
+console.clear();
+/*
+const num1=prompt("Especifique o numero 1: ");
+const num2=prompt("Especifique o numero 2: ");
+const num3=prompt("Especifique o numero 3: ");
+
+let maior;
+if(num1 > num2){
+    maior = num1;
+}else{
+    maior = num2;
+}
+if(maior<num3){
+    maior = num3;
+}
+console.log(maior);
+
+
+const min=prompt("Especifique o minimo: ");
+const max=prompt("Especifique o maximo: ");
+
+let soma;
+let i;
+for(i = min; i <=max; i++){
+    i+=soma;
+}
+console.log(i);
+*/
